f1e799251f9156df5702fda669a2964df62ea9d5
John McCutchen <purplegrits@yahoo.com>
2019-12-04 21:33:08 -0500

    fixed inquirer prompts and constructors

--- app.js
+++ app.js
@@ -2,36 +2,122 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Engineer = require("./lib/Engineer.js")
-const Manager = require("./lib/Manager.js")
-const Intern = require("./lib/Intern.js")
-const Employee = require("./lib/Employee.js")
+const Engineer = require("./lib/Engineer.js");
+const Manager = require("./lib/Manager.js");
+const Intern = require("./lib/Intern.js");
+const path = require("path");
+// const render = require("render")
+const outputPath = path.resolve(__dirname, "output", "team.html");
 
-// questions for user
+IDArray = [];
+teamArray = [];
 
-function promptUser() {
+// manager prompts
+function promptManager() {
     return inquirer
-
         .prompt([
-            {
-                type: "input",
-                name: "name",
-                message: "Employee name"
-            },            
             {
                 type: "list",
                 name: "role",
-                message: "What job type to enter?",
-                choices: ["Engineer", "Intern", "Manager"]
+                message: "Select manager",
+                choices: ["Manager"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "Enter Manager Name"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "Enter ID number",
+                validate: answer => {
+                    if (IDArray.includes(answer)) {
+                        return "This ID has been used.  Enter a different number";
+                    } else {
+                        return true;
+                    }
+                }
             },
             {
                 type: "input",
                 name: "email",
-                message: "What is the email address for this employee?"
+                message: "What is manager's email address"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is manager's office number?"
+            }
+        ])
+        .then((answers) => {
+            const manager = new Manager(answers.name, answers.role, answers.ID, answers.email, answers.officeNumber);
+            teamArray.push(manager);
+            IDArray.push(answers.ID);
+
+
+            makeTeam()
+
+        });
+
+};
+
+// question to continue
+function makeTeam() {
+    return inquirer
+
+        .prompt([
+            {
+                type: "confirm",
+                name: "makeTeam",
+                message: "Would you like to add a team member?"
             }
         ])
         .then(answers => {
-            
+            if (answers.makeTeam){
+                promptUser();
+            } else {
+                return;
+            }
+        })
+}
+
+// questions to create employee
+function promptUser() {
+    return inquirer
+
+        .prompt([ 
+            {
+                type: "input",
+                name: "name",
+                message: "Employee name"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "Enter ID number",
+                validate: answer => {
+                    if (IDArray.includes(answer)) {
+                        return "This ID has been used.  Enter a different number";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "What employee role to enter?",
+                choices: ["Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the email address?"
+            }
+        ])
+        .then(answers => {
+
             if (answers.role === "Engineer") {
                 inquirer.prompt([
                     {
@@ -40,6 +126,12 @@ function promptUser() {
                         message: "GitHub username?"
                     }
                 ])
+                .then(answers => {
+                    const engineer = new Engineer(answers.name, answers.role, answers.ID, answers.email, answers.github);
+                    teamArray.push(engineer);
+                    IDArray.push(answers.ID);
+                    makeTeam();
+                })
             } else if (answers.role === "Intern") {
                 inquirer.prompt([
                     {
@@ -48,24 +140,21 @@ function promptUser() {
                         message: "Name of school?"
                     }
                 ])
-            } else if (answers.role === "Manager") {
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "officeNumber",
-                        message: "Office number?"
-                    }
-                ])
+                .then(answers => {
+                    const intern = new Intern(answers.name, answers.role, answers.ID, answers.email, answers.school);
+                    teamArray.push(intern);
+                    IDArray.push(answers.ID);
+                    makeTeam();
+                })
             }
-            
-        })
-        
+        }) 
+
 }
 
-promptUser()
+promptManager()
 
-module.exports = promptUser
- 
+module.exports = IDArray, teamArray
+
 
 
 
